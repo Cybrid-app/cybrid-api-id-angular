808aed7e7d7e73815e0c1603616195745d826f41
brent-cybrid <brent@cybrid.app>
2022-04-28 11:39:38 +0000

    v0.1.9

---
+++ model/models.ts
@@ -0,0 +1,6 @@
+export * from './application';
+export * from './applicationList';
+export * from './applicationWithSecretAllOf';
+export * from './applicationWithSecret';
+export * from './postBankApplication';
+export * from './postOrganizationApplication';
